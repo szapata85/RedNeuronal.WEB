356e6768335979bf48b1683dcf1ec3eb2754c40b
szapata85 <chechoz20@gmail.com>
2021-07-19 19:59:01 -0500

    NeuralNetwork stage

--- UpdateColor.js
+++ UpdateColor.js
@@ -1,5 +1,29 @@
+var network = new brain.NeuralNetwork();
+
+network.train([
+    //Fondo Negro (Entrada en 0s) = Texto blanco (salida = 1)
+    {input: {rojo:0, verde:0, azul:0}, output: {color:1}},
+
+    //Fondo Blanco (Entrada en 1s) = Texto negro (salida = 0)
+    {input: {rojo:1, verde:1, azul:1}, output: {color:0}},
+
+    //Fondo Verde (Entrada en 1s) = Texto negro (salida = 0)
+    {input: {rojo:0, verde:1, azul:0}, output: {color:0}}
+])
+
 function update(color){
     //console.log(color.toRGBAString());
     var div = document.getElementById('sitio');
     div.style.background = color.toHEXString();
+
+
+        var entrada = {
+            rojo:color.channels.r/255,
+            verde:color.channels.g/255,
+            azul:color.channels.b/255
+        }
+
+        var resultado = network.run(entrada);
+
+        div.style.color = (resultado.color > .5? 'white' : 'black')
 }
